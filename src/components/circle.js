5566516a46bb83b02a30e12e46bea9a39178fdfd
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-10-19 12:58:43 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,7 +1,12 @@
-// import 
+// import
+import Shape from "./shape";
 
 // declare class
-
+export default class Circle extends Shape {
+  constructor(color) {
+    super.color = color;
+  }
+  calculateArea() {}
+}
 
 // export
-
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
 
 // export class
+export default class Shape {
+  color = null;
+  constructor(color) {
+    this.color = color;
+  }
+  calculateArea() {}
+  drawShape() {}
+}
